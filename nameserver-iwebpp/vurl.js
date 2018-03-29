508da6f4f8c87568059e8576d5479dbba4cc63a6
tomzhou <iwebpp@gmail.com>
2018-03-28 17:14:53 -0700

    Update vurl.js

--- nameserver-iwebpp/vurl.js
+++ nameserver-iwebpp/vurl.js
@@ -1,5 +1,5 @@
 // iWebPP vURL pure JS implementation
-// Copyright (c) 2012-2013 Tom Zhou<zs68j2ee@gmail.com>
+// Copyright (c) 2012-2013 Tom Zhou<iwebpp@gmail.com>
 // Notes: 
 // - vURL has two mode: vHost-based and vPath-based
 // - vURL key always is lower case
