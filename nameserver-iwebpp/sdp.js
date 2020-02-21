eee7392b703f2d8a785250762d24648582716040
tomzhou <iwebpp@gmail.com>
2020-02-21 10:39:14 -0800

    Update sdp.js

--- nameserver-iwebpp/sdp.js
+++ nameserver-iwebpp/sdp.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2013 Tom Zhou<iwebpp@gmail.com>
+// Copyright (c) 2012-present Tom Zhou<iwebpp@gmail.com>
 //
 // session description protocol model:
 // 1. when user login to nameserver, nameserver record user's connection info, session 
