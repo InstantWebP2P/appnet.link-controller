1c2ab0cf199dd452ae542be379061cb74e4d38b7
tom zhou <iwebpp@gmail.com>
2020-03-10 22:44:59 -0700

    fixed gencert

--- tools/genSrvKey.bash
+++ tools/genSrvKey.bash
@@ -31,14 +31,6 @@ rm -rf ./certs/ps-csr.pem
 
 echo "### Generate Proxy server cert done"
 
-### ps *.*.vurl.$1
-openssl genrsa -out ./certs/ps-key-sub1.pem 2048
-openssl req -new -key ./certs/ps-key-sub1.pem -subj "/C=CN/ST=SH/L=SH/CN=*.*.vurl.$1" -out ./certs/ps-csr-sub1.pem
-openssl x509 -req -days 368 -CA ./ca-certs/ca-cert.pem -CAkey ./ca-certs/ca-key.pem -CAcreateserial -in ./certs/ps-csr-sub1.pem -out ./certs/ps-cert-sub1.pem  -extensions v3_req -extfile ./certs/ps-v3.conf
-rm -rf ./certs/ps-csr-sub1.pem
-
-echo "### Generate Proxy server sub-domain cert done"
-
 ### ns
 openssl genrsa -out ./certs/ns-key.pem 2048
 openssl req -new -key ./certs/ns-key.pem -subj "/C=CN/ST=SH/L=SH/CN=$1" -out ./certs/ns-csr.pem
