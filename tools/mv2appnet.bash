224392b3b7aa531eb792bd80faa62b555f201b53
tom zhou <iwebpp@gmail.com>
2020-03-30 17:54:58 -0700

    fixed typo

--- tools/mv2appnet.bash
+++ tools/mv2appnet.bash
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-find . -type f -print0 -name "*.js" -o -name "README.md" -o -name "package.json" | \
-xargs -0 sed -i 's/appnet.io/appnet.io/g; s/AppNet.io/AppNet.io/g; s/appnet.io-ws/appnet.io-ws/g; s/AppNet/AppNet/g'
+find . -type f -name "*.js" -print0 | \
+xargs -0 sed -i 's/iwebpp.io/appnet.io/g; s/iWebPP.io/AppNet.io/g; s/iwebpp.io-ws/appnet.io-ws/g; s/iWebPP/AppNet/g'
+
+find . -type f -name "README.md" -print0 | \
+xargs -0 sed -i 's/iwebpp.io/appnet.io/g; s/iWebPP.io/AppNet.io/g; s/iwebpp.io-ws/appnet.io-ws/g; s/iWebPP/AppNet/g'
+
+find . -type f -name "package.json" -print0 | \
+xargs -0 sed -i 's/iwebpp.io/appnet.io/g; s/iWebPP.io/AppNet.io/g; s/iwebpp.io-ws/appnet.io-ws/g; s/iWebPP/AppNet/g'
 
