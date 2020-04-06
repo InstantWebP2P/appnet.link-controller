ea4982b8512717703f92f27c9fafe1f717acfcf6
tom zhou <iwebpp@gmail.com>
2020-04-06 11:55:39 -0700

    fixed typo

--- tools/mv2appnet.bash
+++ tools/mv2appnet.bash
@@ -5,5 +5,8 @@ xargs -0 sed -i 's/aiworkspace.com/appnet.link/g; s/iwebpp@gmail.com/appnet.link
 find . -type f -name "README.md" -print0 | \
 xargs -0 sed -i 's/aiworkspace.com/appnet.link/g; s/iwebpp@gmail.com/appnet.link@gmail.com/g; s/iwebpp.io/appnet.io/g; s/iWebPP.io/AppNet.io/g; s/iwebpp.io-ws/appnet.io-ws/g; s/iWebPP/AppNet/g'
 
+find . -type f -name "Dockerfile*" -print0 | \
+xargs -0 sed -i 's/aiworkspace.com/appnet.link/g; s/iwebpp@gmail.com/appnet.link@gmail.com/g; s/iwebpp.io/appnet.io/g; s/iWebPP.io/AppNet.io/g; s/iwebpp.io-ws/appnet.io-ws/g; s/iWebPP/AppNet/g'
+
 find . -type f -name "package.json" -print0 | \
 xargs -0 sed -i 's/aiworkspace.com/appnet.link/g; s/iwebpp@gmail.com/appnet.link@gmail.com/g; s/iwebpp.io/appnet.io/g; s/iWebPP.io/AppNet.io/g; s/iwebpp.io-ws/appnet.io-ws/g; s/iWebPP/AppNet/g'
