0d2dcc65601089740a07dd5df218226ec46a5928
tomzhou <appnet.link@gmail.com>
2020-07-01 11:21:53 -0700

    Update cluster.js

--- cluster.js
+++ cluster.js
@@ -1,3 +1,6 @@
+// Cluster implementation based on Raft consensus protocol: https://raft.github.io/
+// Copyright 2020, tom zhou<appnet.link@gmail.com>
 
 'use strict';
 var debug = require('debug')('cluster');
+
